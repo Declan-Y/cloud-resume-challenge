6125b8530b03a2aade09577306c26d45cc69aba9
Declan Young <declan.young95@gmail.com>
2024-07-11 21:55:23 +1000

    add api call to counter

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-window.onload = (event) => {
-    console.log("page is fully loaded");
+
+async function getData() {
+    const url = "https://gwxf3flmph.execute-api.us-east-1.amazonaws.com/dev/counter";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      
+      return json
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+
+
+window.onload = async (event) => {
+    let data =  await getData()
+
+    let counter = document.getElementById("counter")
+    counter.innerText = data
   };
   
